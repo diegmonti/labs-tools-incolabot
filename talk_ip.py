cdde0165fc619ddfb015c754c80b1f9e62b7a619
Diego Monti <diegmonti@gmail.com>
2019-01-25 11:48:11 +0100

    Execute mysql query locally

--- talk_ip.py
+++ talk_ip.py
@@ -4,25 +4,24 @@
 # Modified from: https://it.wikipedia.org/wiki/Utente:BimBot/Scripts#discussioneanonimi3.py
 #
 
-import pywikibot, re, subprocess
+import pywikibot
+import re
+import subprocess
+
 from pywikibot import pagegenerators
 from time import *
 
-start=time()
-
-path = '/data/project/lists/output/itwiki/Discussioni/Discussioni_utenti_anonimi_da_svuotare'
-
-ls = subprocess.Popen(('ls', path), stdout=subprocess.PIPE)
-fileName = subprocess.check_output(('tail', '-1'), stdin=ls.stdout)
-ls.wait()
-fullPath = (path + '/' + fileName).rstrip()
-
+start = time()
 site = pywikibot.Site('it', 'wikipedia')
 
+
 def main():
-    talkpages = pagegenerators.TextfilePageGenerator(fullPath)
+    subprocess.run('mysql --defaults-file=~/replica.my.cnf -h ' +
+                   'itwiki.analytics.db.svc.eqiad.wmflabs -BN < talk_ip.sql > talk_ip.out', shell=True, check=True)
+
+    talkpages = pagegenerators.TextfilePageGenerator('talk_ip.out')
     for talk in talkpages:
-        static=False
+        static = False
         if talk.namespace() != 3 or not talk.exists() or not talk.canBeEdited():
             continue
         pywikibot.output("\n>>>>> " + talk.title() + " <<<<<")
@@ -31,19 +30,23 @@ def main():
 
         if checkStatic(talk.title(withNamespace=False)):
             newtext = u'{{IPcondiviso}}\n' + oldtext
-            talk.put(newtext, u'Bot: aggiungo template IPcondiviso ([[Utente:IncolaBot/FAQ|FAQ]])')
+            talk.put(
+                newtext, u'Bot: aggiungo template IPcondiviso ([[Utente:IncolaBot/FAQ|FAQ]])')
         else:
             newtext = u'{{BenvenutoIP}}'
-            talk.put(newtext, u'Bot: aggiungo template BenvenutoIP ([[Utente:IncolaBot/FAQ|FAQ]])')
+            talk.put(
+                newtext, u'Bot: aggiungo template BenvenutoIP ([[Utente:IncolaBot/FAQ|FAQ]])')
+
 
 def checkStatic(ip):
     response = subprocess.check_output(('dig', '-x', ip, '+short'))
     pywikibot.output('Dig response: ' + response)
     return bool(re.search('[Ss]tatic', response))
 
+
 if __name__ == "__main__":
     try:
         main()
     finally:
-       end=time()
-       print "Run time: ", end-start
+        end = time()
+        print "Run time: ", end-start
